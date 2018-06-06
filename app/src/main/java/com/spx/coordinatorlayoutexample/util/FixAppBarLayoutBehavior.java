fc3c7b53ab6fe7208d45008951af4c6d2ddb96be
shaopengxiang <shaopengxiang@sogou-inc>
2018-06-06 15:56:08 +0800

    增加appbarlayout抖动问题录像

---
+++ app/src/main/java/com/spx/coordinatorlayoutexample/util/FixAppBarLayoutBehavior.java
@@ -0,0 +1,90 @@
+package com.spx.coordinatorlayoutexample.util;
+
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.OverScroller;
+
+import java.lang.reflect.Field;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+
+/**
+ * Workaround AppBarLayout.Behavior for https://issuetracker.google.com/66996774
+ * <p>
+ * See https://gist.github.com/chrisbanes/8391b5adb9ee42180893300850ed02f2 for
+ * example usage.
+ * <p>
+ * Change the package name as you wish.
+ */
+public class FixAppBarLayoutBehavior extends AppBarLayout.Behavior {
+
+    private static final String TAG = "AppBarLayoutBehavior";
+
+    public FixAppBarLayoutBehavior() {
+        super();
+    }
+
+    public FixAppBarLayoutBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // --------------begin added by shaopx -------------
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
+        if (ev.getAction() == ACTION_DOWN) {
+            Object scroller = getSuperSuperField(this, "mScroller");
+            if (scroller != null && scroller instanceof OverScroller) {
+                OverScroller overScroller = (OverScroller) scroller;
+                overScroller.abortAnimation();
+            }
+        }
+
+        return super.onInterceptTouchEvent(parent, child, ev);
+    }
+
+    private Object getSuperSuperField(Object paramClass, String paramString) {
+        Field field = null;
+        Object object = null;
+        try {
+            field = paramClass.getClass().getSuperclass().getSuperclass().getDeclaredField(paramString);
+            field.setAccessible(true);
+            object = field.get(paramClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+    // --------------------------- end
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target,
+                               int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
+                dxUnconsumed, dyUnconsumed, type);
+        stopNestedScrollIfNeeded(dyUnconsumed, child, target, type);
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child,
+                                  View target, int dx, int dy, int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+        stopNestedScrollIfNeeded(dy, child, target, type);
+    }
+
+    private void stopNestedScrollIfNeeded(int dy, AppBarLayout child, View target, int type) {
+        if (type == ViewCompat.TYPE_NON_TOUCH) {
+            final int currOffset = getTopAndBottomOffset();
+            if ((dy < 0 && currOffset == 0)
+                    || (dy > 0 && currOffset == -child.getTotalScrollRange())) {
+                ViewCompat.stopNestedScroll(target, ViewCompat.TYPE_NON_TOUCH);
+            }
+        }
+    }
+}
